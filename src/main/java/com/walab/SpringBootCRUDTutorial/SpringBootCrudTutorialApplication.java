e27caa30f2d8b46974b85b31797a6cbda6538cc8
ParkJiSung <gggg0195@gmail.com>
2024-02-29 05:57:16 +0900

    hide yml

---
+++ src/main/java/com/walab/SpringBootCRUDTutorial/SpringBootCrudTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.walab.SpringBootCRUDTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/walab/SpringBootCRUDTutorial/SpringBootCrudTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walab.SpringBootCRUDTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
